ff9bee6f55324b86d85a2af4f549fad4967c3f06
Trevor Sullivan <pcgeek86@gmail.com>
2014-07-25 12:28:33 -0500

    Added Get-AzureMediaServicesProcessor

---
+++ Get-AzureMediaServicesProcessor.ps1
@@ -0,0 +1,18 @@
+﻿function Get-AzureMediaServicesProcessor {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Name
+       ,[Parameter(Mandatory = $true)]
+        [Microsoft.WindowsAzure.MediaServices.Client.CloudMediaContext] $Context
+    )
+
+    # Obtain a reference to the media encoder
+    $MediaProcessor = ($Context.MediaProcessors.Where({ $PSItem.Name -eq $Name; }) | Sort-Object -Property Version)[-1];
+
+    if (!$MediaProcessor) {
+        throw 'An invalid media processor name was specified. Please choose from one of the following: {0}' -f $Context.MediaProcessors.Name;
+    }
+    
+    return $MediaProcessor;
+}
