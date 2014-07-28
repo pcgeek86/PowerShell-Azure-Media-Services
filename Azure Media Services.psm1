d774a8d5e04b2c3bca3d54db3c6950806fd36ab4
Trevor Sullivan <pcgeek86@gmail.com>
2014-07-28 11:16:55 -0500

    Adding New-AzureMediaServicesJob to exported functions

--- Azure Media Services.psm1
+++ Azure Media Services.psm1
@@ -3,12 +3,14 @@
 . $PSScriptRoot\New-AzureMediaServicesAsset.ps1;
 . $PSScriptRoot\Get-AzureMediaServicesProcessor.ps1;
 . $PSScriptRoot\Add-AzureMediaServicesAssetFile;
+. $PSScriptRoot\New-AzureMediaServicesJob;
 
 $PublicFunction = @(
     'Get-AzureMediaServicesContext';
     'New-AzureMediaServicesAsset';
     'Get-AzureMediaServicesProcessor';
     'Add-AzureMediaServicesAssetFile';
+    'New-AzureMediaServicesJob';
     );
 Export-ModuleMember -Function $PublicFunction;
 
