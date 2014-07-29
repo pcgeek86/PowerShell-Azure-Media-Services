d4dde48d39eeba89d47f063a3f697bc57f7927a3
Trevor Sullivan <pcgeek86@gmail.com>
2014-07-29 00:06:35 -0500

    Adding two test functions for development purposes

---
+++ Tests/Test-GetAzureMediaServicesContext.ps1
@@ -0,0 +1,17 @@
+﻿function Test-GetAzureMediaServicesContext {
+    $VerbosePreference = 'continue';
+
+    $ModuleName = 'Azure Media Services';
+    $AccountName = 'tbsmedia';
+
+    # Remove and reload module
+    Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue;
+    & ('{0}\..\Load Module.ps1' -f $PSScriptRoot);
+
+    #$Context = Get-AzureMediaServicesContext -TokenPath ("$PSScriptRoot\..\MediaServicesToken.json") -FromToken -Verbose;
+    $AccountKey = New-AzureMediaServicesKey -Name $AccountName -KeyType Primary -Force;
+    $Context = Get-AzureMediaServicesContext -AccountName $AccountName -Key $AccountKey;
+    Write-Output -InputObject $Context;
+}
+
+$Context = Test-GetAzureMediaServicesContext;
---
+++ Tests/Test-GetAzureMediaServicesProcessor.ps1
@@ -0,0 +1,20 @@
+﻿function Test-GetAzureMediaServicesProcessor {
+    $VerbosePreference = 'continue';
+
+    $ModuleName = 'Azure Media Services';
+    $AccountName = 'tbsmedia';
+
+    # Remove and reload module
+    Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue;
+    & ('{0}\..\Load Module.ps1' -f $PSScriptRoot);
+
+    # Refresh the Azure Media Services Account's Primary Key (password)
+    $AccountKey = New-AzureMediaServicesKey -Name $AccountName -KeyType Primary -Force;
+    # Create the Azure Media Services Context
+    $Context = Get-AzureMediaServicesContext -AccountName $AccountName -Key $AccountKey;
+
+    $MediaProcessor = $Context | Get-AzureMediaServicesProcessor -Name 'Windows Azure Media Encoder' -Version 3.10;
+    Write-Output -InputObject $MediaProcessor;
+}
+
+$MediaProcessor = Test-GetAzureMediaServicesProcessor;
