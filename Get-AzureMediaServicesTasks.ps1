c59358ea664f7133aaaa243ef4e6805e9b620746
Trevor Sullivan <pcgeek86@gmail.com>
2014-07-29 00:06:10 -0500

    Added Get-AzureMediaServicesTask function

---
+++ Get-AzureMediaServicesTasks.ps1
@@ -0,0 +1,13 @@
+﻿function Get-AzureMediaServicesTask {
+    <#
+    .Parameter Job
+    The Azure Media Services Job that the Tasks will be returned from.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Microsoft.WindowsAzure.MediaServices.Client.IJob] $Job
+    )
+
+    return [Microsoft.WindowsAzure.MediaServices.Client.IJob].GetProperty('Tasks').GetValue($Job);
+}
