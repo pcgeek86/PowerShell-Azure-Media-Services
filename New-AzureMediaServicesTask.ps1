ceb3aaa271c2d4a67cb9ff978bbec774c975c359
Trevor Sullivan <pcgeek86@gmail.com>
2014-07-29 00:08:15 -0500

    Adding New-AzureMediaServicesTask function

---
+++ New-AzureMediaServicesTask.ps1
@@ -0,0 +1,16 @@
+﻿function New-AzureMediaServicesTask {
+    [CmdletBinding()]
+    [OutputType([Microsoft.WindowsAzure.MediaServices.Client.ITask])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Microsoft.WindowsAzure.MediaServices.Client.IJob] $Job
+       ,[Parameter(Mandatory = $true)]
+        [Microsoft.WindowsAzure.MediaServices.Client.IMediaProcessor] $MediaProcessor
+       ,[Parameter(Mandatory = $false)]
+        [string] $Configuration = ''
+       ,[Parameter(Mandatory = $true)]
+        [string] $Name
+    )
+
+    [Microsoft.WindowsAzure.MediaServices.Client.IJob].GetProperty('Tasks').GetValue($Job).AddNew($Name, $Processor, '', [Microsoft.WindowsAzure.MediaServices.Client.TaskOptions]::None);
+}
